2ebd3b5c6cbe20511bd075f2c68d9eb63ecf19b6
George Harrison <georgeharrison@Georges-MacBook-Pro.local>
2021-12-06 11:33:17 -0800

    adding endscreen

---
+++ src/components/endScreen.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function endScreen() {
+  return (
+    <div className="endScreen">
+      <h1 id="congrats">Congrats</h1>
+      <h2 id="score">Score: </h2>
+      <h2 id="score">Personal High Score: </h2>
+      <button>Home</button>
+      <button>Play Again</button>
+    </div>
+  );
+}
